0d671a7f731dc6959ea2c8d05103ee59e99ae7fd
Growddy <eedwardjiang@gmail.com>
2025-12-20 23:52:11 -0500

    Stop tracking .next build output

--- .next/dev/static/chunks/app_service-area_page_tsx_b44ce5f8._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_6079bf6c._.js",
-  "static/chunks/components_8da987d3._.js"
-],
-    source: "dynamic"
-});
